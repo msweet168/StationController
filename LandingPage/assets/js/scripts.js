6188e6b900de4cd7667ab4d084c7933f79f3effd
msweet168 <mitchellsweet168@aol.com>
2018-10-12 23:17:52 -0700

    Added started site pages.

---
+++ LandingPage/assets/js/scripts.js
@@ -0,0 +1,26 @@
+/*
+* Scripts.js
+* Station Controller
+*
+* Created by Mitchell Sweet on 10/11/18
+* Copyright © 2018 Mitchell Sweet. All rights reserved.
+*/
+
+$(document).scroll(function() {
+	var y = $(this).scrollTop();
+	if (y > 280) {
+		$('#navbar').fadeIn();
+	} else {
+		$('#navbar').fadeOut();
+	}
+});
+
+function scrollToTop(scrollDuration) {
+    var scrollStep = -window.scrollY / (scrollDuration / 15),
+        scrollInterval = setInterval(function(){
+        if ( window.scrollY != 0 ) {
+            window.scrollBy( 0, scrollStep );
+        }
+        else clearInterval(scrollInterval); 
+    },15);
+}
